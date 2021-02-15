edbf1186f41f72a5d55b59f1919c4b8313aee873
Adrian Palomares <adrianpal45@gmail.com>
2021-02-15 15:30:31 -0800

    added styling

--- src/app/pages/create-post/create-post.component.spec.ts
+++ src/app/pages/create-post/create-post.component.spec.ts
@@ -1,25 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CreatePostComponent } from './create-post.component';
 
-describe('CreatePostComponent', () => {
-  let component: CreatePostComponent;
-  let fixture: ComponentFixture<CreatePostComponent>;
+fdescribe('CreatePostComponent', () => {
+    let component: CreatePostComponent;
+    let fixture: ComponentFixture<CreatePostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreatePostComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CreatePostComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreatePostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CreatePostComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    it('should have input field for title', () => {
+        const titleIn = fixture.debugElement.query(By.css('input#title-input'))
+            .nativeElement;
+        expect(titleIn.placeholder).toEqual('Title');
+    });
+    it('should have a textarea for text(post content)', () => {
+        const textIn = fixture.debugElement.query(By.css('textarea#text-input'))
+            .nativeElement;
+        expect(textIn.placeholder).toEqual('Text (Optional)');
+    });
+    it('should have a button to post', () => {
+        const postBtn = fixture.debugElement.query(By.css('button#post-button'))
+            .nativeElement;
+        expect(postBtn.innerText).toEqual('Post');
+    });
 });
