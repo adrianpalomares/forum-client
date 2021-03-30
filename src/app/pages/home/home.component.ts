fa0232d153378d763dae996a565cc50613d4dffd
Adrian Palomares <adrianpal45@gmail.com>
2021-03-29 19:07:34 -0700

    types into its own directory

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Post } from '../../core/types';
 import { PostService } from '../../core/services/posts.service';
+import { Post } from '../../core/types';
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
