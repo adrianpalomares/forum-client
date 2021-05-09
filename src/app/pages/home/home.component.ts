85a26f5584b75425052d9e462dbc552318ee8082
Adrian Palomares <adrianpal45@gmail.com>
2021-05-09 00:31:13 -0700

    added paginator

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { PostService } from '../../core/services/posts.service';
 import { Post } from '../../core/types';
 @Component({
@@ -8,13 +9,30 @@ import { Post } from '../../core/types';
 })
 export class HomeComponent implements OnInit {
     posts: Post[];
+    // Paginator
+    length: number;
+    pageSize: number;
+    // pageSizeOptions: number[] = [5, 10, 25, 100];
+
     constructor(private postService: PostService) {}
 
     ngOnInit(): void {
-        // if (localStorage.getItem('token') != null) { // temp change because of tests
-        this.postService
-            .getPosts()
-            .subscribe((response) => (this.posts = response.content));
-        // }
+        // For now a page size of 10 is supported
+        this.pageSize = 10;
+        this.postService.getPosts().subscribe((response) => {
+            this.posts = response.content;
+            this.length = response.totalElements; // Setting totatl pages
+        });
+    }
+
+    handlePageEvent(pageEvent: PageEvent): void {
+        console.log(pageEvent);
+        // Grab page index from pageEvent
+        const pageIndex = pageEvent.pageIndex;
+        // Fetch new page of posts
+        this.postService.getPosts(pageIndex).subscribe((response) => {
+            this.posts = response.content;
+            console.log(response.content);
+        });
     }
 }
