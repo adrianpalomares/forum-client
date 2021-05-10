c69f08b51295985805b5c1fba6c08766d472b5e1
Adrian Palomares <adrianpal45@gmail.com>
2021-05-10 09:35:20 -0700

    like counts update accordingly

--- src/app/pages/home/post-card/post-card.component.ts
+++ src/app/pages/home/post-card/post-card.component.ts
@@ -14,6 +14,7 @@ export class PostCardComponent implements OnInit {
     @Input() post: Post;
     comments: Comment[];
     likes: Like[];
+    likeCount: number;
 
     constructor(
         private commentService: CommentService,
@@ -31,30 +32,48 @@ export class PostCardComponent implements OnInit {
             .getLikesFromPostById(this.post.id)
             .subscribe((response) => {
                 this.likes = response;
+                // Getting the total value of likes, will be negative if there are more dislikes
+                this.updateLikeCount();
             });
     }
 
-    handleLikeClick(): void {
+    /**
+     * Method to update like count
+     */
+    updateLikeCount(): void {
+        this.likeCount =
+            this.likes.filter((like) => like.value === true).length -
+            this.likes.filter((like) => like.value === false).length;
+    }
+
+    /**
+     * Handles the like button click
+     * @param value Whether the Like is a like(true) or dislike(false)
+     */
+    handleLikeAction(value: boolean): void {
         const userId = parseInt(this.authService.getUserId());
-        // Filter for a like with userId
+        // Find if the user has a like object already
         const likesFilteredByUserId = this.likes.filter(
             (like) => like.userId === parseInt(this.authService.getUserId())
         );
-        // Checking if user has already submitted a like or if the like is false then we switch to true
-        if (
-            likesFilteredByUserId.length === 0 ||
-            likesFilteredByUserId[0].value === false
-        ) {
+        // If a user does not have a like
+        if (likesFilteredByUserId.length === 0) {
             // Register the like
             this.postService
-                .addLikeToPost(this.post.id, userId, true)
+                .addLikeToPost(this.post.id, userId, value)
                 .subscribe((response) => {
                     this.likes.push(response);
                 });
+        } else if (likesFilteredByUserId[0].value !== value) {
+            // If the like that the user has is opposite to the one they want to submit. eg. They liked a post but want to dislike it.
+            const likeId = likesFilteredByUserId[0].id;
+            // Make the request then when request is completed we replace old like with new
+            this.postService
+                .addLikeToPost(this.post.id, userId, value, likeId)
+                .subscribe((response) => {
+                    this.likes[0] = response; // Prevents duplicate likes
+                    this.updateLikeCount();
+                });
         }
-    }
-
-    handleDislikeClick(): void {
-        console.log('dislike click');
     }
 }
